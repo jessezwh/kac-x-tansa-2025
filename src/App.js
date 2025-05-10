212bbb6877be2a0aad35c5937c5b804bbbe1035f
Jesse Wanghan <jezzerswastaken@gmail.com>
2025-05-10 20:53:35 +1200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [scores, setScores] = useState([0, 0])
+  const [startstop, setStartstop] = useState(0);
+
+  const stst = ['START!', 'stop.'];
+
+  const toggleS = () => {
+    if (startstop) {
+      setStartstop(0);
+    } else {
+      setStartstop(1);
+    }
+  }
+
+  const handle = () => {
+    return
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <h1>Points</h1>
+      <div className="scoreboard">
+        <div className="score tansa">
+          <h1>TANSA</h1>
+          <span> {scores[0]} </span>
+        </div>
+        <div className="score kac">
+          <h1>KAC</h1>
+          <span> {scores[1]} </span>
+        </div>
+      </div>
+
+      <h1>Control Time</h1>
+      <div className="scorebar">
+        <div className="control tansa">
+          <span>0s</span>
+        </div>
+        <div className="control kac">
+          <span>0s</span>
+        </div>
+      </div>
+
+      <h1>Updaters</h1>
+      <div className="updaters">
+        <div className="updater">
+          <form action={handle} name="tansa">
+            <button type="submit">-</button>
+            <input type="text" />
+            <button type="submit">+</button>
+          </form>
+        </div>
+        <div className="updater">
+          <form action={handle} name="kac">
+            <button type="submit">-</button>
+            <input type="text" />
+            <button type="submit">+</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="start">
+        <button onClick={toggleS}>{stst[startstop]}</button>
+      </div>
+    </div>   
   );
 }
 
